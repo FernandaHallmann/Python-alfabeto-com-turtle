170fc6c6692d5a2b5a589dabfaf26adf8e708657
Fernanda Hallmann <111521249+FernandaHallmann@users.noreply.github.com>
2022-11-19 19:35:19 -0300

    Add files via upload

---
+++ 7-alfabeto-turtle.py
@@ -0,0 +1,264 @@
+import turtle
+
+tela = turtle.Screen()
+
+letra = turtle.Turtle()
+letra.pensize(3)
+
+"""
+-> A
+letra.left(65)
+letra.forward(100)
+letra.right(130)
+letra.forward(60)
+letra.right(115)
+letra.forward(50)
+letra.backward(50)
+letra.left(115)
+letra.forward(40)
+
+-> B
+letra.forward(35)
+letra.circle(25, 180)
+letra.forward(35)
+letra.right(180)
+letra.forward(25)
+letra.circle(25, 180)
+letra.forward(25)
+letra.left(90)
+letra.forward(100)
+
+-> C
+letra.color("white")
+letra.left(45)
+letra.forward(80)
+letra.left(90)
+letra.color("black")
+letra.circle(50, 270)
+
+-> D
+letra.forward(25)
+letra.circle(50, 180)
+letra.forward(25)
+letra.left(90)
+letra.forward(100)
+
+-> E
+letra.forward(70)
+letra.backward(70)
+letra.left(90)
+letra.forward(50)
+letra.right(90)
+letra.forward(70)
+letra.backward(70)
+letra.left(90)
+letra.forward(50)
+letra.right(90)
+letra.forward(70)
+
+-> F
+letra.left(90)
+letra.forward(50)
+letra.right(90)
+letra.forward(70)
+letra.backward(70)
+letra.left(90)
+letra.forward(50)
+letra.right(90)
+letra.forward(70)
+
+-> G
+letra.color("white")
+letra.left(45)
+letra.forward(80)
+letra.left(90)
+letra.color("black")
+letra.circle(50, 310)
+letra.left(95)
+letra.forward(60)
+
+-> H
+letra.left(90)
+letra.forward(100)
+letra.backward(50)
+letra.right(90)
+letra.forward(70)
+letra.right(90)
+letra.backward(50)
+letra.forward(100)
+
+-> I
+letra.left(90)
+letra.forward(100)
+
+-> J
+letra.right(90)
+letra.circle(25, 180)
+letra.forward(75)
+
+-> K
+letra.left(90)
+letra.forward(100)
+letra.backward(50)
+letra.right(40)
+letra.forward(65)
+letra.backward(65)
+letra.right(90)
+letra.forward(75)
+
+-> L
+letra.left(90)
+letra.forward(100)
+letra.backward(100)
+letra.right(90)
+letra.forward(70)
+
+-> M
+letra.left(90)
+letra.forward(100)
+letra.right(150)
+letra.forward(100)
+letra.left(120)
+letra.forward(100)
+letra.right(150)
+letra.forward(100)
+
+-> N
+letra.left(90)
+letra.forward(100)
+letra.right(150)
+letra.forward(115)
+letra.left(150)
+letra.forward(100)
+
+-> O
+letra.circle(50)
+
+-> P
+letra.left(90)
+letra.forward(50)
+letra.right(90)
+letra.forward(25)
+letra.circle(25, 180)
+letra.forward(25)
+letra.left(90)
+letra.forward(100)
+
+-> Q
+letra.circle(50)
+letra.color("white")
+letra.left(90)
+letra.forward(35)
+letra.color("black")
+letra.right(120)
+letra.forward(70)
+
+-> R
+letra.left(90)
+letra.forward(50)
+letra.right(90)
+letra.forward(25)
+letra.circle(25, 180)
+letra.forward(25)
+letra.left(90)
+letra.forward(50)
+letra.left(45)
+letra.forward(70)
+
+-> S
+letra.color("white")
+letra.left(90)
+letra.forward(30)
+letra.color("black")
+letra.right(180)
+letra.circle(30, 180)
+letra.left(60)
+letra.forward(70)
+letra.color("white")
+letra.right(145)
+letra.forward(60)
+letra.color("black")
+letra.left(90)
+letra.circle(30, 180)
+
+-> T
+letra.color("white")
+letra.left(90)
+letra.forward(100)
+letra.color("black")
+letra.right(90)
+letra.forward(70)
+letra.backward(35)
+letra.right(90)
+letra.forward(100)
+
+-> U
+letra.color("white")
+letra.left(90)
+letra.forward(100)
+letra.color("black")
+letra.right(180)
+letra.forward(65)
+letra.circle(35, 180)
+letra.forward(65)
+
+-> V
+letra.color("white")
+letra.left(90)
+letra.forward(100)
+letra.color("black")
+letra.right(150)
+letra.forward(100)
+letra.left(120)
+letra.forward(100)
+
+-> W
+letra.color("white")
+letra.left(90)
+letra.forward(100)
+letra.color("black")
+letra.right(170)
+letra.forward(100)
+letra.left(150)
+letra.forward(90)
+letra.right(140)
+letra.forward(90)
+letra.left(150)
+letra.forward(100)
+
+-> X
+letra.left(55)
+letra.forward(120)
+letra.left(125)
+letra.color("white")
+letra.forward(70)
+letra.color("black")
+letra.left(125)
+letra.forward(120)
+
+-> Y
+letra.color("white")
+letra.backward(35)
+letra.color("black")
+letra.left(90)
+letra.forward(50)
+letra.left(35)
+letra.forward(60)
+letra.backward(60)
+letra.right(70)
+letra.forward(60)
+
+-> Z
+letra.color("white")
+letra.left(90)
+letra.forward(100)
+letra.color("black")
+letra.right(90)
+letra.forward(70)
+letra.right(125)
+letra.forward(120)
+letra.left(125)
+letra.forward(70)
+"""
+
+tela.exitonclick()
